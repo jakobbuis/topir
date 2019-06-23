72ef5259558c270b3ba8955d69848afa5fe008e5
Jakob Buis <jakob@jakobbuis.nl>
2019-06-23 22:45:44 +0200

    experiments on adding a bar graph with Chart.js, bars not visible?

--- public/index.php
+++ public/index.php
@@ -4,7 +4,14 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $database = new PDO('sqlite:../statistics');
 $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$data = $database->query('SELECT * FROM statistics ORDER BY day ASC');
+$data = $database->query('SELECT * FROM statistics ORDER BY day ASC')->fetchAll();
+
+$graph = array_map(function($entry) {
+    return (object) [
+        'x' => $entry['day'],
+        'y' => (int) $entry['completed'],
+    ];
+}, $data);
 
 ?><!DOCTYPE html>
 <html>
@@ -12,21 +19,29 @@ $data = $database->query('SELECT * FROM statistics ORDER BY day ASC');
     <title>Todoist statistics</title>
 </head>
 <body>
-    <table>
-        <thead>
-            <tr>
-                <th>Day</th>
-                <th>Count</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($data as $datum): ?>
-                <tr>
-                    <td><?= $datum['day'] ?></td>
-                    <td><?= $datum['completed'] ?></td>
-                </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
+
+    <canvas id="chart" width="400" height="400"></canvas>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+    <script>
+        new Chart(document.getElementById('chart').getContext('2d'), {
+            type: 'bar',
+            data: <?= json_encode($graph); ?>,
+            options: {
+                scales: {
+                    xAxes: [{
+                        barPercentage: 0.5,
+                        barThickness: 6,
+                        maxBarThickness: 8,
+                        minBarLength: 2,
+                        gridLines: {
+                            offsetGridLines: true
+                        }
+                    }]
+                }
+            }
+        });
+
+    </script>
 </body>
 </html>
