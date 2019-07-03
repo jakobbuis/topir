a6649a9c8663dbf3ec096f0081142542f5f457df
Jakob Buis <jakob@jakobbuis.nl>
2019-07-03 22:12:08 +0200

    limit results to the last 30 days

--- public/graph_data.php
+++ public/graph_data.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../bootstrap.php';
 
-$data = $database->query('SELECT * FROM statistics ORDER BY day ASC')->fetchAll();
+$data = $database->query('SELECT * FROM statistics ORDER BY day ASC LIMIT 30')->fetchAll();
 
 // Construct graph data structure and colour set
 $labels = [];
