01ea7606fb26a9972cb002c8476218c253c5d21d
Jakob Buis <jakob@jakobbuis.nl>
2019-07-07 17:17:44 +0200

    complete import script for overdue tasks

--- import/overdue.php
+++ import/overdue.php
@@ -5,16 +5,20 @@ require_once __DIR__ . '/../bootstrap.php';
 // Get API data
 $token = getenv('TODOIST_API_TOKEN');
 $client = new \GuzzleHttp\Client();
-$response = $client->get('https://todoist.com/api/v8/completed/get_stats?token=' . $token);
+$response = $client->get('https://api.todoist.com/rest/v1/tasks?filter=overdue&token=' . $token);
 $data = json_decode((string) $response->getBody());
+$overdueCount = count($data);
 
-// Prep the query
-$insert = "INSERT OR REPLACE INTO statistics (day, completed) VALUES (:day, :completed)";
-$statement = $database->prepare($insert);
+// Prep the queries
+$count = $database->prepare('SELECT COUNT(*) FROM statistics WHERE day = :day');
+$insert = $database->prepare('INSERT INTO statistics (day, overdue) VALUES (:day, :overdue)');
+$update = $database->prepare('UPDATE statistics SET overdue = :overdue WHERE day = :day');
 
-// Import all rows
-foreach ($data->days_items as $datum) {
-    $statement->bindValue(':day', $datum->date, SQLITE3_TEXT);
-    $statement->bindValue(':completed', $datum->total_completed, SQLITE3_INTEGER);
-    $statement->execute();
+// Check for an existing row
+$yesterday = date('Y-m-d', strtotime('yesterday'));
+$count->execute([':day' => $yesterday]);
+if ($count->fetchColumn() == 0) {
+    $insert->execute([':day' => $yesterday, ':overdue' => $overdueCount]);
+} else {
+    $update->execute([':day' => $yesterday, ':overdue' => $overdueCount]);
 }
