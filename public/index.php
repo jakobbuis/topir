093bab5cc2630eff661ee4f43b25624c731d4d74
Jakob Buis <jakob@jakobbuis.nl>
2019-07-13 15:50:45 +0200

    remove unused footer

--- public/index.php
+++ public/index.php
@@ -5,34 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Todoist statistics</title>
-
-    <style>
-        footer {
-            position: absolute;
-            box-sizing: border-box;
-            bottom: 0;
-            left: 0;
-            background-color: #666;
-            width: 100%;
-            padding: 1em;
-            color: white;
-        }
-    </style>
 </head>
 <body>
-
     <canvas id="chart" width="400" height="400"></canvas>
-
-    <footer>
-        <button id=fullscreen>Fullscreen</button>
-        <button id="refresh">Refresh now</button>
-    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.6.0/dist/chartjs-plugin-datalabels.min.js"></script>
 
     <script>
-        window.pollTimeout = null;
         // Self-calling function to poll for new server data
         function poll() {
             fetch('/graph_data.php').then(function(response) {
@@ -41,19 +21,10 @@
                 chart.data.labels = json.labels;
                 chart.data.datasets[0].data = json.counts;
                 chart.data.datasets[1].data = json.overdue;
-                window.pollTimeout = setTimeout(poll, 1000 * 60);
+                setTimeout(poll, 1000 * 60);
                 chart.update(0);
             });
         }
-
-        document.getElementById('fullscreen').addEventListener('click', function(event) {
-            document.documentElement.requestFullscreen();
-        });
-
-        document.getElementById('refresh').addEventListener('click', function(event) {
-            clearTimeout(window.pollTimeout);
-            poll();
-        });
 
         // Setup the canvas as full-screen
         canvas = document.getElementById('chart');
