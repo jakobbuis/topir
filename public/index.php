7eb2dd69da927131ffc6844b7ee45efd4865957d
Jakob Buis <jakob@jakobbuis.nl>
2019-07-21 14:49:06 +0200

    remove unused vue cdn instance

--- public/index.php
+++ public/index.php
@@ -11,7 +11,6 @@
 <body>
     <canvas id="chart" width="400" height="400"></canvas>
 
-    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0/dist/Chart.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.6.0/dist/chartjs-plugin-datalabels.min.js"></script>
     <script src="/js/app.js"></script>
